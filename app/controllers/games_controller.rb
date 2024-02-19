6c80ed8f3ba753f0901419744e7b4d634858ef25
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-02-19 14:07:00 +0100

    oui

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
   def new
+    @letter_list = []
+    alpha = ("A".."Z").to_a
+    10.times do
+      @letter_list << alpha.sample
+    end
   end
 
   def score
