8d2736553d527ab5a93460ae46d10e800c4c5018
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-02-19 16:58:44 +0100

    finnished app fonctionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,36 @@
+require 'open-uri'
+
+# oui
 class GamesController < ApplicationController
   def new
     @letter_list = []
-    alpha = ("A".."Z").to_a
+    alpha = ('A'..'Z').to_a
     10.times do
       @letter_list << alpha.sample
     end
   end
 
   def score
+    @word = params[:word].upcase.chars
+    @letters = params[:letters].upcase.chars - [' ']
+    if try_letters(@word, @letters)
+      try_word
+    else
+      @result = 'wrong letters'
+    end
+  end
+
+  def try_word
+    found = parse['found']
+    @result = "word doesn't exist in english dictionary"
+    @result = 'word found' if found
+  end
+
+  def try_letters(word, letters)
+    (word - letters).empty? && (letters - word).length <= (10 - word.length)
+  end
+
+  def parse
+    JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read)
   end
 end
